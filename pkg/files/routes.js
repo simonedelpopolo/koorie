8bbe141eacd815aef0c9aa77711a0464624ec109
Simone Del Popolo <simonedelpopolo@outlook.com>
2022-01-31 10:55:33 +0000

    fixup! v0.3.19-alpha ⬇︎
    added ./pkg/files/routes.js to previous commit '500053de'

---
+++ pkg/files/routes.js
@@ -0,0 +1,27 @@
+import { function_ } from 'oftypes'
+import { index } from './routes/index/route.js'
+import { process_exit, routes } from 'koorie'
+
+const routes_list = [
+    { route:'', func:index }
+]
+
+export default async () => {
+    
+    const resolvers = {
+        true:( async ( route, func ) => {
+            routes[ route ] = func
+        } ),
+        false: ( async () => {await process_exit(
+            'koorie-routes',
+            Error( 'koorie-Route-Error' ),
+            105 )} )
+    }
+    
+    let makeRoute
+    for ( const r in routes_list ){
+        makeRoute = await function_( routes_list[ r ].func, resolvers )
+        await makeRoute( routes_list[ r ].route, routes_list[ r ].func )
+    }
+    
+}
