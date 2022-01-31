85b182ef2dcd2dc5e941fc62a2ce2608f71205bc
Simone Del Popolo <simonedelpopolo@outlook.com>
2022-01-31 11:19:28 +0000

    v0.3.19-alpha ⬇︎
    the executable.js file is avoided to be published on npmjs because it will be used the ./pkg/files/index.js as the initializeFile to fork or spin up a single server

--- executable.js
+++ executable.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
-import { flags, routes, server } from './index.js'
-
+import routes from './routes.js'
+import { flags, server } from './index.js'
 // Splicing out from `process.argv` the paths for node and executable.js
 process.argv.splice( 0, 2 )
 
 // Process name.
 process.title = 'koorie'
 
+await routes()
 await server( await flags( process.argv ) )
