3c0cff7347ca72ab36f8252baf846b0800668ebd
Simone Del Popolo <simonedelpopolo@outlook.com>
2022-02-06 09:15:25 +0000

    best solution, at the moment, to install the package

--- pkg/install.js
+++ pkg/install.js
@@ -1,15 +1,31 @@
+import { constants } from 'fs'
 import { parse } from 'json-swiss-knife'
-import { cp, readFile, rm, writeFile } from 'fs/promises'
+import { string_ } from 'oftypes'
+import { access, cp, readFile, rm, writeFile } from 'fs/promises'
 
 await cp( './pkg/files/', '../../', { recursive:true, force:true } ).catch( error => console.log( error ) )
 
-const package_json = await parse( await readFile( './package.json' ) )
+const package_json_koorie = await parse( await readFile( './package.json' ) )
 
-delete package_json.scripts.postinstall
+delete package_json_koorie.scripts.postinstall
 
-const buffer = Buffer.from( JSON.stringify( package_json ) )
+const buffer = Buffer.from( JSON.stringify( package_json_koorie ) )
 await writeFile( './package.json', buffer )
     .catch( error => { throw error } )
+
+const fileExist = await access( '../../package.json', constants.F_OK ).catch( error => error.message )
+if( await string_( fileExist ) ){
+    const package_json_root = {
+        scripts: {
+            'koorie.serve':'node index.js',
+        },
+        type: 'module',
+        dependencies: { 'koorie':'latest' }
+    }
+    
+    await writeFile( '../../package.json', JSON.stringify( package_json_root ), { flag: 'w' } )
+        .catch( error => {throw error} )
+}
 
 const korieIndex = Buffer.from( `#!/usr/bin/env node
 import routes from '../../middleware.js'
