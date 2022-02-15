8212c109163bfd16e24ff6cf235532e3e3e8ce24
Simone Del Popolo <simonedelpopolo@outlook.com>
2022-02-15 19:16:45 +0000

    v0.4.89-experimental ⬇
    commented out.
    added Object [ config ]
    
    removed Object [ flags ] the whole thing of parsing commands, flags and options is now handled by Object [ input ] and its own properties.
    
    read the comments in file.
    
    big changes, ❗ not safe for production!
    test test test
    profiling profiling profiling

--- koorie.js
+++ koorie.js
@@ -1,14 +1,61 @@
 #!/usr/bin/env node
-import { flags, server } from './index.js'
+import { config_get, config_set, entry_point, server } from './index.js'
 import { null_, undefined_ } from 'oftypes'
 
-// Splicing out from `process.argv` the paths for node and koorie.js
+// - splicing out from `process.argv` the paths for node and koorie.js
 process.argv.splice( 0, 2 )
 
-// Process name.
+// - process.title
 process.title = 'koorie'
 
-const server_flags = await flags( process.argv )
+
+/**
+ * This function return the parsed commands, flags and options.
+ * The options to be parsed can come from Object[ config.parser ] or process.argv.
+ *
+ * These options will be checked and given back as an object by Object[ input.entry_point ].
+ *
+ * @returns {object}
+ */
+async function configuration (){
+    let options
+    
+    // Object [ config.parser.get() ]
+    // - reads and parses the content of the file .koorierc.
+    // - emit('read') in case koorierc was found in the root directory of the project.
+    // - emit('proceed') in case koorierc was NOT found in the root directory of the project.
+    config_get()
+    
+    // Object [ config.parser.set() ]
+    // - Promise that always resolve after the event [config.parser.get[read] or config.parser.get[proceed] ] fired.
+    // - when "proceed" it passes to Object [ input.entry_point ] the process.argv.
+    // - when "read" it first checks for the "false_flag" passed in phase of forking then it passes the configuration loaded from .koorierc
+    const config_ = await config_set()
+    
+    if( config_ === 'proceed' ) {
+        
+        const entry_point_run = await entry_point( process.argv )
+        
+        options = await entry_point_run.value
+    }else {
+        
+        let config_args
+        
+        if( process.argv.includes( '--false-flag=true' ) ) {
+            config_.push( '--false-flag=true' )
+            config_args = await entry_point( config_ )
+        }
+        
+        else
+            config_args = await entry_point( config_ )
+        
+        options = await config_args.value
+    }
+    
+    return options
+}
+
+const options = await configuration()
 
 const resolversNull = {
     
@@ -17,25 +64,25 @@ const resolversNull = {
         const resolversUndefined = {
             
             false:( async() => {
-                ( await import( `${ process.cwd() }/${server_flags.middleware || server_flags.ml}` ) ).default()
-                await server( server_flags )
+                ( await import( `${ process.cwd() }/${options.middleware || options.ml}` ) ).default()
+                await server( options )
             } ),
             
             true: ( async () => {
                 ( await import( `${ process.cwd() }/middleware.js` ) ).default()
-                await server( server_flags )
+                await server( options )
             } )
             
         };
         
-        ( await undefined_( server_flags.middleware || server_flags.ml, resolversUndefined ) )()
+        ( await undefined_( options.middleware || options.ml, resolversUndefined ) )()
     } ),
     
     true: ( async () => {
         ( await import( `${ process.cwd() }/middleware.js` ) ).default()
-        await server( server_flags )
+        await server( options )
     } )
 };
 
-( await null_( server_flags, resolversNull ) )()
+( await null_( options, resolversNull ) )()
 
