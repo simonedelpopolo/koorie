e5d600640f154e21ecdd3b766840979c58380ab0
Simone Del Popolo <simonedelpopolo@outlook.com>
2022-02-15 18:37:32 +0000

    v0.4.89-experimental ⬇
    every executable, koorie.js and shell.js, now has its own way to deal with commands, flags and options.
    Object input.entry_point takes care to switch process.title, in this case koorie-shell, and dispatches the parsed flags and commands.
    
    big changes, ❗ not safe for production!
    test test test
    profiling profiling profiling

--- shell.js
+++ shell.js
@@ -1,10 +1,25 @@
 #!/usr/bin/env node
-import { flags } from './index.js'
+import { entry_point, init } from './index.js'
 
-// Splicing out from `process.argv` the paths for node and cli.js
+// Splicing out from `process.argv` the paths for node and shell.js
 process.argv.splice( 0, 2 )
 
 // Process name.
 process.title = 'koorie-shell'
 
-await flags( process.argv )
+const entry_point_run = await entry_point( process.argv )
+
+const shell = await entry_point_run.value
+
+// eslint-disable-next-line default-case
+switch ( shell.command ){
+    
+    // Switcher for recognized commands no need for default case.
+    // Checks have been done at Object input.entry_point
+    case 'init':
+        delete shell.command
+        
+        await init( shell )
+        break
+    
+}
