2366eec4bb8a3e1441ff5b51b2637df6a889bacf
Simone Del Popolo <simonedelpopolo@outlook.com>
2022-02-22 08:38:40 +0000

    removed koorieIncoming to push GET/POST path. added property 'incoming' to the object pushed into koorie.routes.list now if a route handle a REQUEST ( for now just POST/GET ) push the incoming path

--- middleware.js
+++ middleware.js
@@ -1,9 +1,7 @@
-import { koorieIncoming, routes } from './index.js'
+import { routes } from './index.js'
 
 export default async () => {
-    routes.list.push( { route:'', asyncFunction: ( await import( './routes/index/route.js' ) ).index  } )
-    routes.list.push( { route:'index', asyncFunction: ( await import( './routes/index/route.js' ) ).index  } )
-    koorieIncoming.path.push( '' )
-    koorieIncoming.path.push( 'index' )
+    routes.list.push( { route:'', asyncFunction: ( await import( './routes/index/route.js' ) ).index, incoming: ''  } )
+    routes.list.push( { route:'index', asyncFunction: ( await import( './routes/index/route.js' ) ).index, incoming: 'index'  } )
     await routes.set()
 }
