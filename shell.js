7e22c2b3e059c3be57ceaf129ccfc4020c8ae0da
Simone Del Popolo <simonedelpopolo@outlook.com>
2022-02-25 20:06:02 +0000

    everything about ⬇︎
    Object [ koorie.socket ]
    Object [ koorie.fork ]
    Object [ shell.set ]
    initialize.js

--- shell.js
+++ shell.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { entry_point, init } from './index.js'
+import { entry_point, init, set } from './index.js'
 
 // - splicing out from `process.argv` the paths for node and shell.js
 process.argv.splice( 0, 2 )
@@ -11,22 +11,29 @@ const entry_point_run = await entry_point( process.argv )
 
 const shell = await entry_point_run
 
-
 // eslint-disable-next-line default-case
-/*Switch ( shell.command ){
+switch ( Object.entries( shell.command )[ 0 ][ 0 ] ){
     
     // Switcher for recognized commands no need for default case.
     // Checks have been done at Object [ input.entry_point ]
     case 'init':
-        delete shell.command
         
-        await init( shell )
+        
+        // - await init( shell )
+        
+        break
+    
+    case 'set':
+        
+        
+        await set( shell.command.set )
         
         break
     
     case 'route':
-        delete shell.command
+        
+        
         
         break
     
-}*/
+}
