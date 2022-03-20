80e0016af0ba197cf5e71472840dcb649b560434
Simone Del Popolo <simonedelpopolo@outlook.com>
2022-03-20 13:58:27 +0000

    v1.10.7-alpha simplifying things ⇩
    simplified middleware.js & route.js
    files that are used for example purpose 'while developing'
    
    REMOVED ~~ejected_koorie.js~~
    
    to use the **ejected state** consult the EXAMPLES.md @ https://github.com/simonedelpopolo/koorie/blob/main/docs/ejected-server
    
    REMOVED many package.json scripts
    
    only the useful once 'while developing' stay.
    1. nodemon.koorie -> simple server
    2. trying the --secure --socket and --hot flag

--- middleware.js
+++ middleware.js
@@ -1,11 +1,6 @@
-import { Answer, routes_inject, routes_set } from './public.js'
+import { routes_inject, routes_set } from './public.js'
 
 export default async () => {
-    await routes_inject( { route:'', asyncFunction: ( await import( './routes/index/route.js' ) ).index, incoming: ''  } )
-    await routes_inject( { route:'index', asyncFunction: ( await import( './routes/index/route.js' ) ).index, incoming: 'index'  } )
-    await routes_inject( { route:'about', asyncFunction:async() => {
-        return new Answer( good => good( Buffer.from( 'about' ) ) )
-    }  } )
+    await routes_inject( { route:'', asyncFunction: ( await import( './routes/index/route.js' ) ).index  } )
     await routes_set()
-
 }
--- routes/index/route.js
+++ routes/index/route.js
@@ -1,70 +1,10 @@
 import { Answer } from '../../public.js'
-import { undefined_ } from 'oftypes'
 
 /**
  * Route - Index.
  *
- * @param {IncomingMessage} incoming - The given IncomingMessage Object.
- * @param {ServerResponse} outgoing - The given ServerResponse Object.
  * @returns {Promise<{buffer:Buffer}> | {buffer:Buffer}}
  */
-export async function index( incoming, outgoing ){
-
-    if( incoming.method === 'POST' ){
-        let message = await Answer.toPost( await Answer.getQuestion( 'body' ), 'index' || '' )
-
-        if( await undefined_( message.invalid ) === true ){
-
-            return new Answer( ( good ) => {
-
-                outgoing.statusCode = 200
-                outgoing.setHeader( 'content-type', 'application/json' )
-                outgoing.setHeader( 'koorie-api', 'true' )
-                outgoing.statusMessage = 'Ok'
-
-                const outgoingBuffer = Buffer.from( JSON.stringify( { receiver: 'oK' } ) )
-                const incomingBuffer = Buffer.from( JSON.stringify( message ) )
-
-                const responseMessage = {
-                    buffer: outgoingBuffer,
-                    incoming:{
-                        length: Buffer.byteLength( incomingBuffer ),
-                        payload: incomingBuffer
-                    }
-                }
-                good( responseMessage )
-            } )
-        }
-    }
-
-    return new Answer( ( good, bad ) => {
-
-        if( incoming.method === 'GET' ) {
-
-            outgoing.statusCode = 200
-            outgoing.statusMessage = 'Ok'
-            outgoing.setHeader( 'koorie-api', 'true' )
-            outgoing.setHeader( 'content-type', 'application/json' )
-
-            const response = { route_index:'response' }
-            const buffer = Buffer.from( JSON.stringify( response ) )
-
-            good( {
-                buffer: buffer,
-            } )
-        }
-        else {
-
-            outgoing.statusCode = 500
-            outgoing.statusMessage = 'Only GET request here ;)'
-            outgoing.setHeader( 'koorie-api', 'false' )
-            outgoing.setHeader( 'content-type', 'application/json' )
-
-            const failed = { message: 'Only GET request here ;)' }
-            const buffer = Buffer.from( JSON.stringify( failed ) )
-
-            bad( buffer )
-        }
-
-    } )
+export async function index( ){
+    return new Answer( good => good( JSON.stringify( { 'index-route' : 'response' } ).toBuffer() ) )
 }
